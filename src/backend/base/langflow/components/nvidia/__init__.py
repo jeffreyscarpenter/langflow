6f0c075ccc82b05548079d2d373551a27cbe2822
Jeffrey Carpenter <jeffrey.carpenter@datastax.com>
2024-12-06 15:41:53 -0700

    initial versions of NVIDIA components

--- src/backend/base/langflow/components/nvidia/__init__.py
+++ src/backend/base/langflow/components/nvidia/__init__.py
@@ -1,13 +1,13 @@
-from .nemo_customizer import NVIDIANeMoCustomizerComponent
-from .nemo_evaluator import NVIDIANeMoEvaluatorComponent
-from .nemo_guardrails import NVIDIANeMoGuardrailsComponent
-from .nvidia_ingest import NVIDIAIngestComponent
 from .nvidia_rerank import NvidiaRerankComponent
+from .nvidia_ingest import NVIDIAIngestComponent
+from .nemo_guardrails import NVIDIANemoGuardrailsComponent
+from .nemo_customizer import NVIDIANemoCustomizerComponent
+from .nemo_evaluator import NVIDIANemoEvaluatorComponent
 
 __all__ = [
-    "NVIDIAIngestComponent",
-    "NVIDIANeMoCustomizerComponent",
-    "NVIDIANeMoEvaluatorComponent",
-    "NVIDIANeMoGuardrailsComponent",
     "NvidiaRerankComponent",
+    "NVIDIAIngestComponent"
+    "NVIDIANemoGuardrailsComponent",
+    "NVIDIANemoCustomizerComponent",
+    "NVIDIANemoEvaluatorComponent"
 ]
